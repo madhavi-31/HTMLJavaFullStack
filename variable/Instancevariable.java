2484f99012b81bcff10e4a44ed75e55c8de35e84
madhavi-31 <madhaviloya31@gmail.com>
2025-06-17 14:24:29 +0530

    Add files via upload

---
+++ variable/Instancevariable.java
@@ -0,0 +1,23 @@
+//InstanceVariable
+public class Instancevariable
+{
+int i;
+float f;
+double d;
+char c;
+boolean bo;
+long l;
+byte b;
+short s;
+public static void main (String[] args)
+{
+System.out.println(i);
+System.out.println(f);
+System.out.println(d);
+System.out.println(c);
+System.out.println(bo);
+System.out.println(s);
+System.out.println(l);
+System.out.println(b);
+}
+}
---
+++ variable/Staticvariable.java
@@ -0,0 +1,23 @@
+//Static Variable
+public class Staticvariable
+{
+static int i;
+static float f;
+static double d;
+static char c;
+static boolean bo;
+static long l;
+static byte b;
+static short s;
+public static void main (String[] args)
+{
+System.out.println(i);
+System.out.println(f);
+System.out.println(d);
+System.out.println(c);
+System.out.println(bo);
+System.out.println(s);
+System.out.println(l);
+System.out.println(b);
+}
+}
