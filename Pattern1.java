f5b2a74b1953502f35e6cc8fb67548f4707da1ab
madhavi-31 <madhaviloya31@gmail.com>
2025-06-20 12:31:10 +0530

    Pattern1.java

--- Pattern1.java
+++ Pattern1.java
@@ -229,3 +229,183 @@ System.out.println(str.substring(0,i));
 }
 }
 }
+//Pattern-11
+/*public class  Pattern1
+{
+public static void main(String[] args)
+{
+int n=5;
+for(int i=1;i<=n;i++)
+{
+for(int j=i;j<n;j++)
+{
+System.out.print(" ");
+}
+for(int k=1;k<=(2*i-1);k++)
+{
+System.out.print("*");
+}
+System.out.println();
+}
+}
+}
+Output:
+       *
+     ***
+   *****
+  *******
+**********/
+ 
+
+
+//Pattern-12
+/*public class Pattern1
+{
+public static void main(String[] args)
+{
+int n=4;
+for(int i=1;i<=n;i++)
+{
+for(int j=i;j<n;j++)
+{
+System.out.print("  ");
+}
+for(int j=1;j<=(2*i-1);j++)
+{
+System.out.print("* "); 
+}
+System.out.println( );
+}
+for(int i=n-1;i>=1;i--)
+{
+for(int j=n;j>i;j--)
+{
+System.out.print("  ");
+}
+for(int j=1;j<=(2*i-1);j++)
+{
+System.out.print("* "); 
+}
+System.out.println("  "); 
+}
+}
+}
+Output:
+        *
+     * * *
+   * * * * *
+* * * * * * *
+   * * * * *
+     * * *
+        **/
+
+
+
+//Pattern-13
+/*public class Pattern1
+{
+ public static void main(String[] args) 
+{
+int n = 4;
+ for (int i = 0; i < n; i++)
+{
+for (int j = 0; j < n - i - 1; j++)
+System.out.print(" ");
+System.out.print("*");
+if (i != 0)
+{
+for (int j = 0; j < 2 * i - 1; j++)
+System.out.print(" ");
+System.out.print("*");
+}
+System.out.println();
+}
+for (int i = n - 2; i >= 0; i--) 
+{
+ for (int j = 0; j < n - i - 1; j++)
+System.out.print(" ");
+System.out.print("*");
+if (i != 0) 
+{
+ for (int j = 0; j < 2 * i - 1; j++)
+System.out.print(" ");
+ System.out.print("*");
+}
+System.out.println();
+}
+}
+}
+Output:
+    *
+  * *
+ *   *
+*     *
+ *   *
+  * *
+   **/
+           
+
+
+//Pattern-14
+/*class Pattern1
+{
+public static void main(String[] args) 
+{
+int n = 5;
+for (int i = 1; i <= n; i++) 
+{
+for (int j = 1; j <= n; j++)
+{
+ if (i == 1 || i == n || j == 1 || j == n)
+System.out.print("*  ");
+else
+System.out.print("   ");
+}
+System.out.println( );
+}
+}
+}
+Output:
+*  *  *  *  *
+*               *
+*               *
+*               *
+*  *  *  *  **/
+
+
+
+
+
+//Pattern-15
+/*class Pattern1
+{
+ public static void main(String[] args)
+{
+ int n=7;
+int mid=n/2;
+for(int i=0;i<n;i++)
+{
+for(int j=0;j<n;j++)
+{
+if(i==mid||j==mid)
+{
+System.out.print("*");
+}
+ else
+{
+System.out.print(" ");
+}
+}
+System.out.println();
+}
+}
+}     
+Output:           
+       *
+       *
+       *
+   *******
+       *
+       *
+       **/
+
